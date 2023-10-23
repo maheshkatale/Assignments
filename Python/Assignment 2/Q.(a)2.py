42b39f8497a1934096608a52480557d74ff870ec
maheshkatale <148619752+maheshkatale@users.noreply.github.com>
2023-10-23 23:52:08 +0530

    Create Q.(a)2.py

---
+++ Python/Assignment 2/Q.(a)2.py
@@ -0,0 +1,9 @@
+"""
+2) define a function which accepts a number and return its square.
+"""
+
+def sqr(num):
+    return num**2
+
+num=int(input("Enter the number : "))
+print("Square is",sqr(num))
