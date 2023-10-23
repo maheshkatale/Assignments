7f6d771ec23936750127fa8b59d30ec2e100897f
maheshkatale <148619752+maheshkatale@users.noreply.github.com>
2023-10-23 23:58:45 +0530

    Delete Assignment 2 directory

--- Assignment 2/Q.2.py
+++
@@ -1,9 +0,0 @@
-"""
-2) define a function which accepts a number and return its square.
-"""
-
-def sqr(num):
-    return num**2
-
-num=int(input("Enter the number : "))
-print("Square is",sqr(num))
