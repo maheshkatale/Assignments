f8dd3b0e15b87ae18f1c33e4cbdb0e047370ee2b
maheshkatale <148619752+maheshkatale@users.noreply.github.com>
2023-10-24 22:33:07 +0530

    Create Q.1.py

---
+++ Python/Assignment 4/dictionary/Q.1.py
@@ -0,0 +1,6 @@
+"""
+1) accept 5 students name and store them in the dictionary by providing keys from 1 to 5 respectively.
+"""
+dict={1:"Akshay",2:"Vicky",3:"Ranveer",4:"Kushal",5:"Nagraj"}
+
+print(dict)
