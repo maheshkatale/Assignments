680548f3478b1cbbdab66df43ea6c04f8e813ce1
maheshkatale <148619752+maheshkatale@users.noreply.github.com>
2024-05-23 20:21:56 +0530

    Create 20. Valid Parentheses.py

---
+++ Python/Leetcode_problems/20. Valid Parentheses.py
@@ -0,0 +1,36 @@
+'''
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+Example 3:
+
+Input: s = "(]"
+Output: false
+'''
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        bracket_map={')':'(', ']':'[', '}':'{'}
+        stack = []
+        for char in s:
+            if char in bracket_map:
+                top_element = stack.pop() if stack else '#'
+                if bracket_map[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+        return not stack
